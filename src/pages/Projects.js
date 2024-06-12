8a92c0380bfd61153a0e39237903e26dd71737f1
Sakilshikdar <sakilshikdar2504@gmil.com>
2024-06-12 19:19:04 +0600

    update-project-homepage

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Project.css'
+import pic from '../assets/images/web_image/web_1_1.png'
+import pic2 from '../assets/images/web_image/web_2_1.png'
+import pic3 from '../assets/images/web_image/web_3_1.png'
 
 const Projects = () => {
     return (
@@ -9,11 +12,11 @@ const Projects = () => {
             <div className='grid sm:grid-cols-2 lg:grid-cols-3 mt-7 lg:pl-20 gap-4'>
                 <div className=" card w-96 bg-base-100 shadow-xl">
                     <div>
-                        <img src="https://i.ibb.co/K24vysH/Screenshot-439.png" className="landing max-w-sm rounded-lg shadow-lg bg-base-200 p-3" />
+                        <img src={pic} className="landing max-w-sm rounded-lg shadow-lg bg-base-200 p-3" />
                     </div>
                     <div class="card-body">
-                        <h2 class="card-title">wedding-photography</h2>
-                        <p><span className='font-bold text-2xl'>This</span> is a wedding photographer webside.This webside have login and register page ans have checkout component ans protected routs</p>
+                        <h2 class="card-title">Quiz Master</h2>
+                        <p><span className='font-bold text-2xl'>This</span> is a Qize related webside.This webside have login and register page ans have checkout component ans protected routs</p>
                         <Link to='/projectOne'> <div class="card-actions justify-end">
                             <button class="btn btn-primary">Web Details</button>
                         </div></Link>
@@ -21,11 +24,11 @@ const Projects = () => {
                 </div>
                 <div class="card w-96 bg-base-100 shadow-xl">
                 <div>
-                        <img src="https://i.ibb.co/WG8YwGb/Screenshot-440.png" className= "landing max-w-sm rounded-lg shadow-lg bg-base-200 p-3" />
+                        <img src={pic2} className= "landing max-w-sm rounded-lg shadow-lg bg-base-200 p-3" />
                     </div>
                     <div class="card-body">
-                        <h2 class="card-title">Warehouse-management</h2>
-                        <p><span className='font-bold text-2xl'>Its</span> a wearhouse-management webside.4 components have this webside and some protected routes .If you not a login or resister user you cannot access this routes.Home page have Updated button if you click the button you go the other page..</p>
+                        <h2 class="card-title">Ecommarce Webside</h2>
+                        <p><span className='font-bold text-2xl'>Its</span> a Ecommarce-management webside.4 components have this webside and some protected routes .If you not a login or resister user you cannot access this routes.Home page have Updated button if you click the button you go the other page..</p>
                         <Link to='/projectTwo'>
                             <div class="card-actions justify-end">
                                 <button class="btn btn-primary">Web Details</button>
@@ -35,11 +38,11 @@ const Projects = () => {
                 </div>
                 <div class="card w-96 bg-base-100 shadow-xl">
                 <div>
-                        <img src="https://i.ibb.co/nMHxnjs/Screenshot-449.png" className="landing max-w-sm rounded-lg shadow-lg bg-base-200 p-3" />
+                        <img src={pic3} className="landing max-w-sm rounded-lg shadow-lg bg-base-200 p-3" />
                     </div>
                     <div class="card-body">
-                        <h2 class="card-title">Manufacturer-webside</h2>
-                        <p><span className='font-bold text-2xl'>P</span>rojects name is Tools manufactures wevside and have some components.Dashboard components is a protected route and this components have some section.  Blog and Portfolio page show some intersting thing.</p>
+                        <h2 class="card-title">Recipi Managmet</h2>
+                        <p><span className='font-bold text-2xl'>P</span>rojects name is Recipi managment webside and have some components.Dashboard components is a protected route and this components have some section.  Blog and Portfolio page show some intersting thing.</p>
                         <div class="card-actions justify-end">
                             <Link to='/projectsThree'><button class="btn btn-primary">Web Details</button></Link>
                         </div>
